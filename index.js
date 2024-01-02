f3d84337dab9010ae4c239641badf588bd54e299
Balakrishnan <balakrishnan@neosme.com>
2024-01-02 12:02:42 +0530

    added console

--- index.js
+++ index.js
@@ -69,6 +69,7 @@ app.post('/user/create', async (req, res, next) => {
 
 app.get('/user/list/real', async (req, res, next) => {
   try {
+    console.log("Enter into list real data")
     const realdata = await User1.find()
     res.status(200).send(realdata)
   } catch (Eror) {
