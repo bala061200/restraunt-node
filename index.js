702e9bf6088442e78a1af062f2cea24f0279dc6e
Balakrishnan <balakrishnan@neosme.com>
2024-01-02 12:13:15 +0530

    cors changes

--- index.js
+++ index.js
@@ -4,11 +4,27 @@ const app = express()
 const PORT = 4000;
 const cors = require("cors");
 
+
+// const corsOptions = {
+//   origin: 'https://restraunt-client.vercel.app',
+//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+// };
+
+
+
+// app.use(cors({
+//   "origin": "*",
+//   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   "preflightContinue": false,
+//   "optionsSuccessStatus": 204
+// }));
+
+
 app.use(cors({
-  "origin": "*",
+  "origin": 'https://restraunt-client.vercel.app',
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
-  "optionsSuccessStatus": 204
+  "optionsSuccessStatus": 200
 }));
 
 const crypto = require('crypto');
